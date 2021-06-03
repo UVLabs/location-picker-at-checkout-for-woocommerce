0c87da0378988a6c25231b412d8dfd5b553f586a
UVLabs <uriahs.victor@gmail.com>
2021-06-03 13:33:12 -0400

    move public folder to inside includes folder

--- includes/class-lpac.php
+++ includes/class-lpac.php
@@ -131,14 +131,14 @@ class Lpac {
 		 * The class responsible for defining all actions that occur in the public-facing
 		 * side of the site.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-lpac-public.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/public/class-lpac-public.php';
 
 
 		/**
 		 * The class responsible for defining all actions that occur in the public-facing
 		 * side of the site.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/partials/class-lpac-public-display.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/public/partials/class-lpac-public-display.php';
 
 		$this->loader = new Lpac_Loader();
 
